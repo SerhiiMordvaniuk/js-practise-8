9d4b6f650b276163462f0a133f74430c859aaa4e
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-14 20:07:12 +0200

    kkk

---
+++ js/js-8.js
@@ -0,0 +1,46 @@
+"use strict"
+// _.sum([2, 5])
+//  console.log(_.sum([2, 5]));
+ 
+
+//  const dog = {
+//   name: "Mango",
+//   age: 3,
+//   isGoodBoy: true,
+// };
+
+// const lalala = JSON.stringify(dog)
+
+// console.log(lalala);
+
+
+// const jjss = lalala.pa
+
+
+// const tututu = JSON.stringify(32);
+// console.log(tututu);
+// console.log(32);
+
+// const lololo = {
+//   name: "Mango",
+//   age: 3,
+//     isGoodBoy: true,
+//   user : "User",
+//   bark() {
+//     console.log("Woof!");
+//   },
+// };
+
+
+// const ljjbojb = JSON.stringify(lololo)
+
+
+try {
+    const sss = JSON.parse("Hello")
+}
+catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+
+    
+}
